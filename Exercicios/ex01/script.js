48a5529c48393ff664f3e9aaf586bd105dfbb1ae
Matheusnasc0903 <matheusnasc0903@gmail.com>
2023-05-25 06:31:43 -0300

    modified:   Exercicios/ex01/script.js

--- Exercicios/ex01/script.js
+++ Exercicios/ex01/script.js
@@ -5,12 +5,12 @@ function carregar(){
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >= 5 && hora < 12){
-
+        img.src = "manha.png"
     }
     else if(hora >= 12 && hora <= 18){
-
+        img.src = 'tarde.png'
     }
     else{
-        
+        img.src = 'noite.png'
     }
 }
