7a5c7ec41348e101dc3c92a6a1e4d71946d80d56
Matheusnasc0903 <matheusnasc0903@gmail.com>
2023-05-26 06:55:49 -0300

    modified:   Exercicios/ex01v2/script.js

--- Exercicios/ex01v2/script.js
+++ Exercicios/ex01v2/script.js
@@ -1,3 +1,7 @@
 var date = new Date()
 var hours = date.getHours()
 var minutes = date.getMinutes()
+var clock = document.getElementById('clock')
+var image = document.getElementById('iamge')
+
+clock.innerHTML = `${hours}:${minutes}`
