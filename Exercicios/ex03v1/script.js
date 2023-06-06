eda6136be6bed6f4b1065cb440943c2b9fb7706f
Matheusnasc0903 <matheusnasc0903@gmail.com>
2023-06-06 06:59:03 -0300

    modified:   Exercicios/ex03v1/index.html 	modified:   Exercicios/ex03v1/script.js 	modified:   Exercicios/ex03v1/style.css

---
+++ Exercicios/ex03v1/script.js
@@ -0,0 +1,16 @@
+function contar(){
+    let comeco = Number(document.querySelector("#num-inicio").value)
+    let fim = Number(document.querySelector("#num-fim").value)
+    let passos = Number(document.querySelector("#passos").value)
+    let saida = document.querySelector("#saida")
+    if (comeco < fim){
+        for (let i = comeco; i <= fim; i += passos){
+            saida.innerHTML += `${i} `
+        }
+    }else{
+        for (let i = comeco; i >= fim; i -= passos) {
+            saida.innerHTML += `${i} `
+        }
+    }
+    saida.innerHTML += `\u{1F3C1}`
+}
