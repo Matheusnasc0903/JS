cfc86e70cedec0cc51079aec5b6a676b8e1d8b22
Matheusnasc0903 <matheusnasc0903@gmail.com>
2023-06-07 14:37:00 -0300

    Atualização Tabuada
    Alterei a forma de mostrar os resultados.
    Antes: <select>Conteúdo Dinâmico JS</select>
    Depois: <ol>Conteúdo Dinâmico JS</ol>

--- Exercicios/ex04v1/script.js
+++ Exercicios/ex04v1/script.js
@@ -5,10 +5,11 @@ function calcular(){
     tab.innerHTML=''
     for (let i = 0; i < 10; i++) {
         let res = mult * i
-        let item = document.createElement('option')
-        item.text = `${mult} x ${i} = ${res}`
+        let item = document.createElement('li')
+        item.innerHTML = `${mult} x ${i} = ${res}`
         item.value = `tab${i}`
         tab.appendChild(item)
+        item.style.listStyle="none"
     }
     saida.style.display="flex"
 }
